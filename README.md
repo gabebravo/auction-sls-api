7c0ac67db8754ebc1ed21fcc8f548b96716f50f1
Gabe Bravo <gabecbravo@gmail.com>
2021-02-28 17:09:44 -0500

    Added middy middleware and also added some declarative error handling

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 
 - [serverless-pseudo-parameters plugin](https://www.npmjs.com/package/serverless-pseudo-parameters): Allows you to take advantage of CloudFormation Pseudo Parameters.
 - [serverless-bundle plugin](https://www.npmjs.com/package/serverless-pseudo-parameters): Bundler based on the serverless-webpack plugin - requires zero configuration and fully compatible with ES6/ES7 features.
+- [middy: AWS Lambda Middleware](https://github.com/middyjs/middy)
+- [http errors](https://github.com/jshttp/http-errors#readme)
--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
@@ -12,15 +17,22 @@ async function createAuction(event, context) {
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
+    x,
   };
 
   // chaining .promise makes any aws opreation async/await in nature
-  await dynamoDb
-    .put({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction,
-    })
-    .promise();
+  try {
+    await dynamoDb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    // declarative error handling w/ createError
+    console.log('error', error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -28,4 +40,8 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+// Apply Middy middlware functions
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
